eea5686810d3c360c201e0859cfdbf916372a3c0
Techipeeyon <techiepeeyon@gmail.com>
2022-03-29 22:23:36 +0530

    Updated

--- app/src/main/java/com/example/project_chatapplication/Chat.java
+++ app/src/main/java/com/example/project_chatapplication/Chat.java
@@ -2,13 +2,29 @@ package com.example.project_chatapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Chat extends AppCompatActivity {
+    private Button profileButton, logoutbutton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        profileButton = findViewById(R.id.profileButton);
+        logoutbutton = findViewById(R.id.logoutbutton);
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainIntent = new Intent(Chat.this,Profile.class);
+                startActivity(mainIntent);
+            }
+        });
     }
 }
