40c96ef9bcd1dbcfcbc150d11bb1d4d8622977ff
Techipeeyon <techiepeeyon@gmail.com>
2022-03-29 23:30:59 +0530

    Updated

--- app/src/main/java/com/example/project_chatapplication/Chat.java
+++ app/src/main/java/com/example/project_chatapplication/Chat.java
@@ -7,14 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Chat extends AppCompatActivity {
-    private Button profileButton, logoutbutton;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class Chat<mAuth> extends AppCompatActivity {
+    private Button profileButton, logoutbutton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        mAuth = FirebaseAuth.getInstance();
 
         profileButton = findViewById(R.id.profileButton);
         logoutbutton = findViewById(R.id.logoutbutton);
@@ -23,6 +27,15 @@ public class Chat extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent mainIntent = new Intent(Chat.this,Profile.class);
+                startActivity(mainIntent);
+            }
+        });
+        logoutbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                finish();
+                Intent mainIntent = new Intent(Chat.this,Login.class);
                 startActivity(mainIntent);
             }
         });
--- app/src/main/java/com/example/project_chatapplication/Profile.java
+++ app/src/main/java/com/example/project_chatapplication/Profile.java
@@ -1,14 +1,59 @@
 package com.example.project_chatapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.annotations.NotNull;
 
 public class Profile extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private TextView Name, About, phoneno, DoB;
+    private DatabaseReference userref;
+    String email;
+    String currentUserID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Name = findViewById(R.id.name);
+        About = findViewById(R.id.about);
+        phoneno = findViewById(R.id.phoneno);
+        DoB = findViewById(R.id.DoB);
+
+        mAuth = FirebaseAuth.getInstance();
+        email = getIntent().getStringExtra("email");
+        currentUserID = mAuth.getCurrentUser().getUid();
+        userref = FirebaseDatabase.getInstance().getReference("Users").child(currentUserID);
+        userref.child(email).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    String UName = snapshot.child("Name").getValue().toString();
+                    Name.setText(UName);
+                }
+                else{
+                    Name.setText(email);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
     }
 }
